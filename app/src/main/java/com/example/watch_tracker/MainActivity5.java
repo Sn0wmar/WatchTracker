ace860436f8a1b2fd46dc95218aede1c670e9dfe
Sn0wmar <alexandre.heurtaut@edu.esiee.fr>
2023-10-17 15:34:52 +0200

    Commit modif

---
+++ app/src/main/java/com/example/watch_tracker/MainActivity5.java
@@ -0,0 +1,17 @@
+package com.example.watch_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity5 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+}
+}
+
