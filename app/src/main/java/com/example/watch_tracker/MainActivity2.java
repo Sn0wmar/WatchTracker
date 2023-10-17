be1c6c938b161e519ea7eae257683b0284e8a8a8
Sn0wmar <alexandre.heurtaut@edu.esiee.fr>
2023-10-17 14:28:52 +0200

    Initial commit

--- app/src/main/java/com/example/watch_tracker/MainActivity2.java
+++ app/src/main/java/com/example/watch_tracker/MainActivity2.java
@@ -2,13 +2,19 @@ package com.example.watch_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity2 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageView mask = findViewById(R.id.masquer);
+        mask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(it);
+            }
+        });
     }
 }
