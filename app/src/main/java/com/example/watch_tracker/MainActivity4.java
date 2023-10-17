0a4350e5143a4568f849fd9979801d0775060a84
Sn0wmar <alexandre.heurtaut@edu.esiee.fr>
2023-10-17 15:15:40 +0200

    Initial commit

--- app/src/main/java/com/example/watch_tracker/MainActivity4.java
+++ app/src/main/java/com/example/watch_tracker/MainActivity4.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main4);
 
-        ImageView mask = findViewById(R.id.masquer3);
+        ImageView mask = findViewById(R.id.masquer4);
         mask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it3 = new Intent(getApplicationContext(), MainActivity4.class);
-                startActivity(it3);
+                Intent it4 = new Intent(getApplicationContext(), MainActivity5.class);
+                startActivity(it4);
             }
         });
     }
