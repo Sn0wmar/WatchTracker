d1699c499bbbbe29be599f60d43c9d10cb4d4fed
Sn0wmar <alexandre.heurtaut@edu.esiee.fr>
2023-10-24 15:36:20 +0200

    Commit modif boutons vos ami

--- app/src/main/java/com/example/watch_tracker/vos_amis.java
+++ app/src/main/java/com/example/watch_tracker/vos_amis.java
@@ -2,7 +2,10 @@ package com.example.watch_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class vos_amis extends AppCompatActivity {
 
@@ -10,5 +13,56 @@ public class vos_amis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vos_amis);
+
+        ImageView mask = findViewById(R.id.pas_vu);
+        // news
+        ImageView mask2 = findViewById(R.id.en_cours);
+        ImageView mask3 = findViewById(R.id.vu);
+        ImageView mask4 = findViewById(R.id.profil);
+        ImageView mask5 = findViewById(R.id.liste);
+
+
+        mask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Pas_visionne.class);
+                startActivity(it);
+            }
+        });
+
+        mask2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), en_cours.class);
+                startActivity(it);
+            }
+        });
+
+        mask3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), visionne.class);
+                startActivity(it);
+
+            }
+        });
+
+        mask4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), profil.class);
+                startActivity(it);
+            }
+        });
+
+        mask5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Liste_personnelle.class);
+                startActivity(it);
+
+            }
+        });
+
     }
 }
