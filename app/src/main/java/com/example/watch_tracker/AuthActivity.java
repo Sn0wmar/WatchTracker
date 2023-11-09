00ac421d7df970f3754d953985d7b980e561a9ad
inm77 <esnault.remi@laposte.net>
2023-11-09 11:37:23 +0100

    Inscription

--- app/src/main/java/com/example/watch_tracker/AuthActivity.java
+++ app/src/main/java/com/example/watch_tracker/AuthActivity.java
@@ -1,14 +1,18 @@
 package com.example.watch_tracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,8 +46,7 @@ public class AuthActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AuthActivity.this, InscriptionActivity.class);
-                startActivity(intent);
+                registerUser();
             }
         });
     }
@@ -58,14 +61,39 @@ public class AuthActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
-                            // L'authentification a réussi, vous pouvez rediriger l'utilisateur vers l'activité souhaitée.
-                            // Par exemple, Liste_personnelle.
                             Intent intent = new Intent(AuthActivity.this, Liste_personnelle.class);
                             startActivity(intent);
                         } else {
-                            // L'authentification a échoué, afficher un message d'erreur.
                             Toast.makeText(AuthActivity.this, "Échec de l'authentification", Toast.LENGTH_SHORT).show();
                         }
+                    }
+                });
+    }
+
+    private void registerUser() {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(AuthActivity.this, "Inscription réussie", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Ajoute ces lignes pour afficher le message d'erreur spécifique dans les logs.
+                            Log.e("Inscription", "Échec de l'inscription", task.getException());
+                            Toast.makeText(AuthActivity.this, "Échec de l'inscription : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Ajoute ces lignes pour afficher le message d'erreur spécifique dans les logs.
+                        Log.e("Inscription", "Échec de l'inscription", e);
+                        Toast.makeText(AuthActivity.this, "Échec de l'inscription : " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
