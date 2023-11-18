268aa90703a0c3e328f476ad54db8b2c4f123b97
inm77 <esnault.remi@laposte.net>
2023-11-18 09:10:54 +0100

    ajout des boutons dans add

--- app/src/main/java/com/example/watch_tracker/Bouton.java
+++ app/src/main/java/com/example/watch_tracker/Bouton.java
@@ -1,8 +1,13 @@
 package com.example.watch_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.watch_tracker.Movie;
@@ -20,6 +25,64 @@ public class Bouton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bouton);
+
+        ImageView mask = findViewById(R.id.pas_vu);
+        ImageView mask2 = findViewById(R.id.en_cours);
+        ImageView mask3 = findViewById(R.id.vu);
+        ImageView mask4 = findViewById(R.id.profil);
+        ImageView mask5 = findViewById(R.id.liste);
+
+
+        mask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Pas_visionne.class);
+                startActivity(it);
+            }
+        });
+
+        mask2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), en_cours.class);
+                startActivity(it);
+            }
+        });
+
+        mask3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), visionne.class);
+                startActivity(it);
+
+            }
+        });
+
+        mask4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), profil.class);
+                startActivity(it);
+            }
+        });
+
+        mask5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Liste_personnelle.class);
+                startActivity(it);
+
+            }
+        });
+
+
+
+
+
+
+
+
+
 
         // Initialisation de la RecyclerView
         recyclerView = findViewById(R.id.rv_movies);
