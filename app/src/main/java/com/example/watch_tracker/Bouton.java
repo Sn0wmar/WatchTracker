b36c9bd4f6fffcc25a50156c43834389fb7c0e42
inm77 <esnault.remi@laposte.net>
2023-11-18 11:27:30 +0100

    correction bug RV add

--- app/src/main/java/com/example/watch_tracker/Bouton.java
+++ app/src/main/java/com/example/watch_tracker/Bouton.java
@@ -1,16 +1,15 @@
 package com.example.watch_tracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.watch_tracker.Movie;
 import com.example.watch_tracker.MovieResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,6 +19,7 @@ public class Bouton extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RVAdapter rvAdapter;
+    private ConstraintLayout constraintLayout; // Ajout de la vue racine
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,6 @@ public class Bouton extends AppCompatActivity {
         ImageView mask3 = findViewById(R.id.vu);
         ImageView mask4 = findViewById(R.id.profil);
         ImageView mask5 = findViewById(R.id.liste);
-
 
         mask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +53,6 @@ public class Bouton extends AppCompatActivity {
             public void onClick(View view) {
                 Intent it = new Intent(getApplicationContext(), visionne.class);
                 startActivity(it);
-
             }
         });
 
@@ -71,27 +69,41 @@ public class Bouton extends AppCompatActivity {
             public void onClick(View view) {
                 Intent it = new Intent(getApplicationContext(), Liste_personnelle.class);
                 startActivity(it);
-
             }
         });
-
-
-
-
-
-
-
-
-
 
         // Initialisation de la RecyclerView
         recyclerView = findViewById(R.id.rv_movies);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // Ajout de la vue racine
+        constraintLayout = findViewById(R.id.your_root_layout_id);
+
         // Appel de la méthode pour obtenir les films populaires
         getPopularMovies();
 
+        // Ajout de l'écouteur de changement de hauteur de fenêtre
+        constraintLayout.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            private int previousHeight;
 
+            @Override
+            public boolean onPreDraw() {
+                int height = constraintLayout.getHeight();
+                if (height != previousHeight) {
+                    // La hauteur de la fenêtre a changé
+                    boolean isKeyboardVisible = height < previousHeight;
+                    if (isKeyboardVisible) {
+                        // Le clavier est affiché, masquez la RecyclerView
+                        recyclerView.setVisibility(View.GONE);
+                    } else {
+                        // Le clavier n'est pas affiché, montrez la RecyclerView
+                        recyclerView.setVisibility(View.VISIBLE);
+                    }
+                    previousHeight = height;
+                }
+                return true;
+            }
+        });
     }
 
     private void getPopularMovies() {
@@ -104,7 +116,6 @@ public class Bouton extends AppCompatActivity {
                     // Mettez à jour la RecyclerView avec les films populaires
                     rvAdapter = new RVAdapter(Bouton.this, response.body().getResults());
                     recyclerView.setAdapter(rvAdapter);
-
                 } else {
                     Log.e("Bouton", "Erreur de réponse : " + response.message());
                 }
@@ -117,3 +128,4 @@ public class Bouton extends AppCompatActivity {
         });
     }
 }
+
