4ea981a8b10d9abb223d882c2993893715b113d0
inm77 <esnault.remi@laposte.net>
2023-11-22 13:53:55 +0100

    correction bug

--- app/src/main/java/com/example/watch_tracker/Bouton.java
+++ app/src/main/java/com/example/watch_tracker/Bouton.java
@@ -1,12 +1,15 @@
 package com.example.watch_tracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,13 +37,82 @@ public class Bouton extends AppCompatActivity implements RVAdapter.OnItemClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bouton);
 
-
-
         // Initialisation de la RecyclerView
         recyclerView = findViewById(R.id.rv_movies);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         constraintLayout = findViewById(R.id.Bouton);
         searchField = findViewById(R.id.searchField);
+
+        // Ajout de la détection d'appui sur les boutons
+        ImageView mask = findViewById(R.id.pas_vu);
+        ImageView mask2 = findViewById(R.id.en_cours);
+        ImageView mask3 = findViewById(R.id.vu);
+        ImageView mask4 = findViewById(R.id.profil);
+        ImageView mask5 = findViewById(R.id.liste);
+
+        mask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Pas_visionne.class);
+                startActivity(it);
+            }
+        });
+
+        mask2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), en_cours.class);
+                startActivity(it);
+            }
+        });
+
+        mask3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), visionne.class);
+                startActivity(it);
+            }
+        });
+
+        mask4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), profil.class);
+                startActivity(it);
+            }
+        });
+
+        mask5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Liste_personnelle.class);
+                startActivity(it);
+            }
+        });
+
+        // Ajout d'un écouteur de focus pour cacher le clavier lorsqu'il y a un focus sur l'EditText
+        searchField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    // Cacher le clavier si l'EditText perd le focus
+                    hideKeyboard();
+                }
+            }
+        });
+
+        // Ajout d'un écouteur de touche pour détecter l'appui sur la touche "Done"
+        searchField.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == KeyEvent.KEYCODE_SEARCH) {
+                    // Cacher le clavier si la touche "Done" est pressée
+                    hideKeyboard();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         getTrendingContent();
 
@@ -58,7 +130,6 @@ public class Bouton extends AppCompatActivity implements RVAdapter.OnItemClickLi
                 return true;
             }
         });
-
 
         searchField.addTextChangedListener(new TextWatcher() {
             @Override
@@ -123,5 +194,14 @@ public class Bouton extends AppCompatActivity implements RVAdapter.OnItemClickLi
         Intent intent = new Intent(Bouton.this, FilmDetailsActivity.class);
         intent.putExtra("movie", movie);
         startActivity(intent);
+    }
+
+    // Méthode pour cacher le clavier
+    private void hideKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
