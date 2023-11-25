2123aee22d2e784aac8ddb3decbda050d11482d7
inm77 <esnault.remi@laposte.net>
2023-11-25 22:43:04 +0100

    corrction bug preparation suite

--- app/src/main/java/com/example/watch_tracker/profil.java
+++ app/src/main/java/com/example/watch_tracker/profil.java
@@ -1,26 +1,44 @@
 package com.example.watch_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import java.util.ArrayList;
+import java.util.List;
 
-public class profil extends AppCompatActivity {
+public class profil extends AppCompatActivity implements RVAdapter.OnItemClickListener {
+
+    private RecyclerView recyclerView;
+    private RVAdapter rvAdapter;
+    private List<Movie> movieList;
+    private ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profil);
 
         ImageView mask = findViewById(R.id.pas_vu);
-        // news
+
         ImageView mask2 = findViewById(R.id.en_cours);
         ImageView mask3 = findViewById(R.id.vu);
         ImageView mask4 = findViewById(R.id.profil);
         ImageView mask5 = findViewById(R.id.liste);
         ImageView mask6 = findViewById(R.id.ami);
-        ImageView mask7 = findViewById(R.id.bouton_plus3);    //
+        //ImageView mask7 = findViewById(R.id.bouton_plus3);
 
         mask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,13 +91,86 @@ public class profil extends AppCompatActivity {
         });
 
 
-        mask7.setOnClickListener(new View.OnClickListener() {
+        /*mask7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent it = new Intent(getApplicationContext(), Bouton.class);
                 startActivity(it);
             }
+        });*/
+        recyclerView = findViewById(R.id.rv_movies);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        movieList = new ArrayList<>();
+        rvAdapter = new RVAdapter(this, movieList, this);
+        recyclerView.setAdapter(rvAdapter);
+
+
+
+        recyclerView = findViewById(R.id.rv_movies);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        constraintLayout = findViewById(R.id.profils);
+
+        loadMovies();
+
+        constraintLayout.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            private int previousHeight;
+
+            @Override
+            public boolean onPreDraw() {
+                int height = constraintLayout.getHeight();
+                if (height != previousHeight) {
+                    // La hauteur de la fenêtre a changé
+                    boolean isKeyboardVisible = height < previousHeight;
+                    if (isKeyboardVisible) {
+                        // Le clavier est affiché, masquez la RecyclerView
+                        recyclerView.setVisibility(View.GONE);
+                    } else {
+                        // Le clavier n'est pas affiché, montrez la RecyclerView
+                        recyclerView.setVisibility(View.VISIBLE);
+                    }
+                    previousHeight = height;
+                }
+                return true;
+            }
         });
     }
+
+    private void loadMovies() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            DatabaseReference userMoviesRef = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("movies");
+            userMoviesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    movieList.clear();
+                    for (DataSnapshot movieSnapshot : dataSnapshot.getChildren()) {
+                        Movie movie = movieSnapshot.getValue(Movie.class);
+                        if (movie != null && "oui".equals(movie.getFav())) {
+                            movieList.add(movie);
+                        }
+                    }
+                    rvAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Gestion des erreurs de base de données
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onItemClick(Movie movie) {
+        Intent intent = new Intent(profil.this, FilmDetailsActivity.class);
+        intent.putExtra("movie", movie);
+        startActivity(intent);
+    }
+
 }
+
 
