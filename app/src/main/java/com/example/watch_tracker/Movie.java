7f7f18cbdb464fb911f74025cac7611810935e0e
Ranked <patrick.tran@edu.esiee.fr>
2023-12-03 09:55:26 +0100

    //comm Movie

--- app/src/main/java/com/example/watch_tracker/Movie.java
+++ app/src/main/java/com/example/watch_tracker/Movie.java
@@ -5,7 +5,10 @@ import android.os.Parcelable;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
+//Déclaration de la classe Movie qui implémente l'interface Parcelable
 public class Movie implements Parcelable {
+
+    //Déclaration des variables
     private int id;
 
     @SerializedName(value = "title", alternate = {"name"})
@@ -26,16 +29,17 @@ public class Movie implements Parcelable {
     @SerializedName("seasons")
     private List<Season> seasons;
 
+    //URL de base pour les images des films
     private static final String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
 
 
-
+    //Cosntructeur par défaut
     public Movie() {
 
     }
 
-
+    //Constructeur avec paramètres
     public Movie(int id, String title, String overview, String posterPath) {
         this.id = id;
         this.title = title;
@@ -43,12 +47,14 @@ public class Movie implements Parcelable {
         this.posterPath = posterPath;
     }
 
+    //Méthodes setter pour statut et Fav
     public void setStatut(String statut) {
         this.statut = statut;
     }
 
     public void setFav(String Fav){this.Fav = Fav;}
 
+    //Méthode getter pour id, tittle, overview, posterPath, statut, Fav et seasons
     public int getId() {
         return id;
     }
@@ -75,11 +81,12 @@ public class Movie implements Parcelable {
         return seasons;
     }
 
+    //Méthode setter pour seasons
     public void setSeasons(List<Season> seasons) {
         this.seasons = seasons;
     }
 
-
+    //Constructeur prenant un Parcel pour la création d'un objet Movie
     protected Movie(Parcel in) {
         id = in.readInt();
         title = in.readString();
@@ -87,6 +94,7 @@ public class Movie implements Parcelable {
         posterPath = in.readString();
     }
 
+    //Créateur Parcelable pour l'objet Movie
     public static final Creator<Movie> CREATOR = new Creator<Movie>() {
         @Override
         public Movie createFromParcel(Parcel in) {
@@ -99,11 +107,14 @@ public class Movie implements Parcelable {
         }
     };
 
+    //Méthode pour décrire le contenu de l'objet Parcelable
     @Override
     public int describeContents() {
         return 0;
     }
 
+
+    //Méthode pour écrire les valeurs de l'objet dans un Parcel
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeInt(id);
