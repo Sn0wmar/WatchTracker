983a6ff2bd294bf8e016b30cac22afab07343049
Ranked <patrick.tran@edu.esiee.fr>
2023-12-03 09:26:07 +0100

    //comm InformationsActivity

--- app/src/main/java/com/example/watch_tracker/InformationsActivity.java
+++ app/src/main/java/com/example/watch_tracker/InformationsActivity.java
@@ -7,14 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
 
+//Déclaration de la classe InformationsActivity qui étend AppCompatActivity
 public class InformationsActivity extends AppCompatActivity {
 
+    //Méthode appellée lors de la création de l'activité
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Définition du layout
         setContentView(R.layout.informations);
 
+        //Récupération de la référence ImageView du bouton
         ImageView mask = findViewById(R.id.Bouton_retour);
+
+        //Définition d'un écouteur de clic sur le bouton retour
         mask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
