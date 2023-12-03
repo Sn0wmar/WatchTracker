e9ee7ca96ad30a1fb440b6a201e3b001248574e7
Ranked <patrick.tran@edu.esiee.fr>
2023-12-03 10:03:26 +0100

    //comm MovieResponse

--- app/src/main/java/com/example/watch_tracker/MovieResponse.java
+++ app/src/main/java/com/example/watch_tracker/MovieResponse.java
@@ -3,15 +3,18 @@ package com.example.watch_tracker;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
+//D2claration de la classe MovieResponse
 public class MovieResponse {
     @SerializedName("results")
     private List<Movie> results;
 
 
-
+//Méthode getter pour récupérer la liste de films
     public List<Movie> getResults() {
         return results;
     }
+
+    //Méthode setter pour défiir la liste de films
     public void setResults(List<Movie> results) {
         this.results = results;
     }
