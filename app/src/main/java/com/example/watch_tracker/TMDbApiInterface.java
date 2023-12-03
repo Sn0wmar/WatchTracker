700b02182ce853f48300a16771046551207daf0f
Ranked <patrick.tran@edu.esiee.fr>
2023-12-03 10:40:12 +0100

    //comm TMDbApiInterface

--- app/src/main/java/com/example/watch_tracker/TMDbApiInterface.java
+++ app/src/main/java/com/example/watch_tracker/TMDbApiInterface.java
@@ -6,7 +6,10 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
+// Interface décrivant les points d'accès de l'API TMDb
 public interface TMDbApiInterface {
+
+    // Méthode pour récupérer le contenu tendance de la semaine
     @GET("trending/all/week")
     Call<MovieResponse> getTrendingContent(
             @Query("api_key") String apiKey,
@@ -14,6 +17,7 @@ public interface TMDbApiInterface {
             @Query("language") String language
     );
 
+    // Méthode pour récupérer les détails d'un film ou d'une série spécifique
     @GET("media/{id}")
     Call<MovieResponse> getMovieDetails(
             @Path("id") int idFilm,
@@ -21,6 +25,7 @@ public interface TMDbApiInterface {
             @Query("language") String language
     );
 
+    // Méthode pour effectuer une recherche de contenu multi-catégorie
     @GET("search/multi")
     Call<MovieResponse> searchAllContent(
             @Query("api_key") String apiKey,
